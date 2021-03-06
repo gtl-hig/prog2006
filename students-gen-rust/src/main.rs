1b2842ebf7f7bc796e52ff8158e2dcab4489d776
Carl Findahl <sportleroro@gmail.com>
2021-03-06 09:44:59 +0100

    Make student generation a bit more interesting and able to cover more cases

--- students-gen-rust/src/main.rs
+++ students-gen-rust/src/main.rs
@@ -1,62 +1,90 @@
 extern crate clap;
 extern crate std;
 
+use anyhow::{Context, Result};
 use clap::{App, Arg};
 use std::fs::File;
 use std::io::Write;
-use anyhow::Result;
+use rand::Rng;
+
+// Some arrays of names to produce more combinations of good / bad names
+const OK_FIRST_NAMES: [&str; 5] = ["Xi", "Petter", "Ola", "Vilde", "John"];
+const OK_SURNAMES: [&str; 4] = ["Wang", "Pettersen", "Tronsmoen", "Baker"];
+const BAD_FIRST_NAMES: [&str; 4] = ["x", "per", "o", "F"];
+const BAD_SURNAMES: [&str; 3] = ["Wan", "pettersen", "T"];
 
 fn main() -> Result<()> {
     let matches = App::new("Students Data Generator")
         .version("1.0")
         .author("Mariusz fom PROG2006")
         .about("The program generates data file for testing student error handling implementations")
-        .arg(Arg::with_name("correct")
-            .short("c")
-            .long("correct")
-            .value_name("CORRECT")
-            .help("How many correct students to generate")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("incorrect")
-            .short("i")
-            .long("incorrect")
-            .value_name("INCORRECT")
-            .help("How many incorrect students to generate")
-            .takes_value(true)
-            .required(true))
-        .arg(Arg::with_name("output")
-            .short("o")
-            .long("output")
-            .value_name("OUTPUT_FILE")
-            .help("Output filename to generate the data INTO.")
-            .takes_value(true)
-            .default_value("output.txt")
-            .required(true))
+        .arg(
+            Arg::with_name("correct")
+                .short("c")
+                .long("correct")
+                .value_name("CORRECT")
+                .help("How many correct students to generate")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("incorrect")
+                .short("i")
+                .long("incorrect")
+                .value_name("INCORRECT")
+                .help("How many incorrect students to generate")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("OUTPUT_FILE")
+                .help("Output filename to generate the data INTO.")
+                .takes_value(true)
+                .default_value("output.txt")
+                .required(true),
+        )
         .get_matches();
 
-    let mut correct: i32 = matches.value_of("correct").unwrap().parse().unwrap();
-    let mut incorrect: i32 = matches.value_of("incorrect").unwrap().parse().unwrap();
+    // Parse arguments with Clap, add error context to help the user if they mess up using anyhow
+    let correct: i32 = matches
+        .value_of("correct").unwrap()
+        .parse()
+        .with_context(|| "Correct should be a number")?;
+    let incorrect: i32 = matches
+        .value_of("incorrect").unwrap()
+        .parse()
+        .with_context(|| "Incorrect should be a number")?;
     let path = matches.value_of("output").unwrap();
 
-    println!("Generating {} students: {} correct - {} incorrect",
-             correct + incorrect, correct, incorrect);
+    println!(
+        "Generating {} students: {} correct - {} incorrect",
+        correct + incorrect,
+        correct,
+        incorrect
+    );
 
-    let mut output = File::create(path).unwrap();
 
-    loop {
-        if correct == 0 { break }
-        write!(output, "new Aaaa Aaaa 20\n").unwrap();
-        correct -= 1;
+    let mut output = File::create(path)?;
+    let mut rng = rand::thread_rng();
+
+    // Generate the OK students
+    for _ in 0..correct {
+        let name: &str = OK_FIRST_NAMES[(rng.gen_range(0..OK_FIRST_NAMES.len()))];
+        let surname: &str = OK_SURNAMES[(rng.gen_range(0..OK_SURNAMES.len()))];
+        writeln!(output, "new {} {} {}", name, surname, rng.gen_range(18..=130))?;
     }
 
-    loop {
-        if incorrect == 0 { break }
-        write!(output, "new 1 1 a\n").unwrap();
-        incorrect -= 1;
+    // Generate the BAD students
+    for _ in 0..incorrect {
+        let name: &str = BAD_FIRST_NAMES[(rng.gen_range(0..BAD_FIRST_NAMES.len()))];
+        let surname: &str = BAD_SURNAMES[(rng.gen_range(0..BAD_SURNAMES.len()))];
+        writeln!(output, "new {} {} {}", name, surname, rng.gen_range(0..18))?;
     }
 
-    write!(output, "list\nend\n").unwrap();
+    write!(output, "list\nend\n")?;
 
     Ok(())
 }
